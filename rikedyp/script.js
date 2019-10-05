9334ea859114b37dc1c642ded20a2a2e7d298ff1
rikedyp <rikedyp@users.noreply.github.com>
2019-10-05 08:24:54 +0100

    Flickity carousel

--- rikedyp/script.js
+++ rikedyp/script.js
@@ -27,33 +27,9 @@ var slideIndex = 1;
 window.onload = function() {  
   document.body.style.transition = "background " + tTime + "s";
   var intervalID = window.setInterval(bgFade, tDelay);
-  imgSlide(1);
 }
 
 function bgFade(){
   var col = Math.floor(Math.random() * bgcolors.length);
   document.body.style.background = bgcolors[col];  
 }
-
-// Image slideshow
-function imgSlide(dir) {
-  var i;
-  var x = document.getElementsByClassName("slide");
-  slideIndex = (slideIndex + dir) % x.length;
-  var slidePrev = slideIndex - 1;
-  var slideNext = slideIndex + 1;
-  if (slidePrev < 0) {slidePrev = x.length - 1;}
-  if (slideNext > x.length - 1) {slideNext = 0;}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-    x[i].style.left = "200vw"; 
-  }
-  x[slideIndex].style.left = "0vw";
-  x[slideIndex].style.display = "inline";
-  x[slidePrev].style.display = "inline";
-  x[slideNext].style.display = "inline";
-  x[slidePrev].style.left = "150vw";
-  x[slideNext].style.left = "-150vw";
-  // x[slideIndex-1].style.display = "inline";  
-}
-
